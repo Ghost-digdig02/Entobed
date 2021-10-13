f9b245bc964d7d73905f9691d68d2e5fe36f7e14
beeb7 <74414637+beeb7@users.noreply.github.com>
2021-10-13 16:18:14 +0200

    jump :D

--- Entombed/Assets/Scripts/PlayerAndCamera/playerMovement.cs
+++ Entombed/Assets/Scripts/PlayerAndCamera/playerMovement.cs
@@ -8,6 +8,7 @@ public class playerMovement : MonoBehaviour
 
     public float speed = 12f;
     public float gravity = -9.81f;
+    public float jumpHeight = 3f;
 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
@@ -35,8 +36,17 @@ public class playerMovement : MonoBehaviour
 
         controller.Move(move * speed * Time.deltaTime);
 
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+
+        }
+
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
+
+
     }
 }
