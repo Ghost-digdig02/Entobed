9536a8c144e40c24a2fb1fab8960eee8eab8f7c7
PotatoPig64 <74412624+PotatoPig64@users.noreply.github.com>
2021-10-15 18:46:59 +0200

    Inventory part 2 Item database

--- Entombed/Assets/ScriptableObjects/Inventory/Scripts/InventoryObject.cs
+++ Entombed/Assets/ScriptableObjects/Inventory/Scripts/InventoryObject.cs
@@ -3,39 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Inventory", menuName = "Inventory Syste/Inventory" )]
-public class InventoryObject : ScriptableObject
+public class InventoryObject : ScriptableObject, ISerializationCallbackReceiver
 {
     public ItemDatabaseObject database;
     public List<InventorySlot> Container = new List<InventorySlot>(); //a list that contains all the items in the inventory
     public void AddItem(ItemObject _item, int _amount)
     {
-        bool hasItem;
-        hasItem = false; //here we asume that we don't have the item in our inventory
-
+        
         for(int i = 0; i < Container.Count; i++)//here we check if we have the item in the inventory
         {
             if(Container[i].item == _item)
             {
-                Container[i].AddAmount(_amount);
-                hasItem = true;
-                break;
+               Container[i].AddAmount(_amount);
+                return;              
             }          
-        }
-        if (!hasItem) //is used if the item does not already exist in the inventory
-        {
-            Container.Add(new InventorySlot(_item, _amount));
-        }
+        }    
+        Container.Add(new InventorySlot(database.GetId[_item], _item, _amount));
+        
+    }
+
+    public void OnAfterDeserialize() // is used as soon as something changes on a scriptable object that cuses unity to need to serialize that object
+    {
+        for (int i = 0; i < Container.Count; i++) //looks through all the items in our container and makes sure that all item matches with their item id
+        { 
+            Container[i].item = database.GetItem[Container[i].ID]; 
+        } 
+    }
+
+    public void OnBeforeSerialize()
+    {
+        
     }
 }
 
 [System.Serializable]
 public class InventorySlot
 {
+    public int ID;
     public ItemObject item;
     public int amount; //how many of the items that is in the inventory
 
-    public InventorySlot(ItemObject _item, int _amount) //a constructor for the class
+    public InventorySlot( int _id, ItemObject _item, int _amount) //a constructor for the class
     {
+        ID = _id;
         item = _item;
         amount = _amount;
 
--- Entombed/Assets/ScriptableObjects/Items/Scripts/ItemDatabaseObject.cs
+++ Entombed/Assets/ScriptableObjects/Items/Scripts/ItemDatabaseObject.cs
@@ -11,19 +11,22 @@ public class ItemDatabaseObject : ScriptableObject, ISerializationCallbackReceiv
     [TextArea(15, 20)]
     public string description; //holds a description of the item
     public ItemObject[] Items;
-    public Dictionary<ItemObject, int> GetId = new Dictionary<ItemObject, int>();
+    public Dictionary<ItemObject, int> GetId = new Dictionary<ItemObject, int>(); //here I use two dictionaries (for the purpose of the item database), I know that this requires more memory(we're using double the memory) than using a doubble for-loop (which I could have used instead), but preformancewise it wont matter for this smaller game (using doubble for-loops takes a lot of time)
+    public Dictionary<int, ItemObject> GetItem = new Dictionary<int, ItemObject>();
 
     public void OnAfterDeserialize() //is used since Unity doesn't serialize dictionaries
     {
         GetId = new Dictionary<ItemObject, int>();
-        for(int i = 0; i < Items.Length; i++)
+        GetItem = new Dictionary<int, ItemObject>();
+        for (int i = 0; i < Items.Length; i++)
         {
             GetId.Add(Items[i], i);
+            GetItem.Add(i, Items[i]);
         }
     }
 
     public void OnBeforeSerialize()
     {
-        
+        //this is not used, but it still needs to be here since we use ISerializationCallbackReceiver :)
     }
 }
