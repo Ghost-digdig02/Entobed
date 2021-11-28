41ee281655683dc3ba047074e8e3c657a5921629
PotatoPig64 <74412624+PotatoPig64@users.noreply.github.com>
2021-11-28 16:01:57 +0100

    Pick Up Items update
    You can now pick up items, and they will be added to your inventory.
    It does need some fine tuning though lol

--- Entombed/Assets/ScriptableObjects/Inventory/Scripts/PlayerInventory.cs
+++ Entombed/Assets/ScriptableObjects/Inventory/Scripts/PlayerInventory.cs
@@ -29,9 +29,21 @@ public class PlayerInventory : MonoBehaviour
         {
             inventory.Load();
         }
+
     }
 
-    private void OnApplicationQuit()
+    public void OnTriggerEnter(Collider other)
+    {
+        var item = other.GetComponent<GroundItem>();
+        if (item)
+        {
+            Item _item = new Item(item.item);
+            inventory.AddItem(_item, 1);
+            Destroy(other.gameObject);
+        }
+    }
+
+        private void OnApplicationQuit()
     {
        inventory.Container.Items = new InventorySlot[18];
     }
--- Entombed/Assets/Scripts/PickUpObjects/PickUpObjects.cs
+++ Entombed/Assets/Scripts/PickUpObjects/PickUpObjects.cs
@@ -9,36 +9,46 @@ public class PickUpObjects : MonoBehaviour
     public GameObject item;
     public GameObject tempParent;
     public Transform guide;
+    private bool mouseDown;
     
-    public Collider col;
+    public Collider guideCol;
     
     void Start()
     {
         item.GetComponent<Rigidbody>().useGravity = false;
+        mouseDown = false;
     }
 
     private void FixedUpdate()
     {
-        if (Input.GetMouseButton(2)) //adds the item to the player's inventory if you middle click
+        if (mouseDown == true && Input.GetMouseButton(2)) //adds the item to the player's inventory if you middle click
         {
-            PlayerInventory.addItemToInventory();
+            Debug.Log("middleclick");
+            guideCol.enabled = true;
         }
     }
 
     void OnMouseDown()
     {
+        mouseDown = true;
+        guideCol.enabled = false;
+
         item.GetComponent<Rigidbody>().useGravity = false;
         item.GetComponent<Rigidbody>().isKinematic = true;
         item.transform.position = guide.transform.position;
         item.transform.rotation = guide.transform.rotation;
         item.transform.parent = tempParent.transform;
+        Debug.Log("mouse down");
     }
 
     void OnMouseUp()
     {
+        mouseDown = false;
+        guideCol.enabled = true;
         item.GetComponent<Rigidbody>().useGravity = true;
         item.GetComponent<Rigidbody>().isKinematic = false;
         item.transform.parent = null;
         item.transform.position = guide.transform.position;
+        Debug.Log("mouse up");
     }
 }
