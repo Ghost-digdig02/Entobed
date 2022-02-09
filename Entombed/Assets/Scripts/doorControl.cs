1f41ea0244b60d369baaf7c8eac987d330d8f21f
beeb7 <74414637+beeb7@users.noreply.github.com>
2022-02-09 13:46:27 +0100

    Update doorControl.cs

--- Entombed/Assets/Scripts/doorControl.cs
+++ Entombed/Assets/Scripts/doorControl.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class doorControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] private Animator door = null;
+
+    [SerializeField] private bool openTrigger = false;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+
+        if (other.CompareTag("Player"))
+        {
+            if (openTrigger)
+            {
+                door.Play("doorOpening", 0, 0.0f);
+                //gameObject.SetActive(false);
+            }
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
